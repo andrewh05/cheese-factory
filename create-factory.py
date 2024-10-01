2d043a40074efc59cd51e1d23ad5758e41ecc47d
andrew haddad <88251285+andrewh05@users.noreply.github.com>
2024-10-01 21:48:22 +0300

    Create create-factory.py
    create the  database only

---
+++ create-factory.py
@@ -0,0 +1,22 @@
+import mysql.connector
+cn = mysql.connector.connect( host='localhost',
+                            user='root',
+                            password='' )
+
+cr = cn.cursor()
+
+sq = 'create database Factory'
+cr.execute(sq)
+
+sq1 = sq = '''create table Factory.Product 
+                ( Code varchar(10) not null,
+                Descrip varchar(30) not null,
+                Catg varchar(20) not null,
+                Qtty int not null,
+                Exp_Date date not null,
+                primary key (code) ) '''
+
+cr.execute(sq1)
+
+
+cn.close()
