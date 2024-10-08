11d14f586d207cdac4234be2ab1dea8001d99145
andrew haddad <88251285+andrewh05@users.noreply.github.com>
2024-10-08 18:01:16 +0300

    Update db.php

--- cheese_factory/db.php
+++ cheese_factory/db.php
@@ -1,13 +1,15 @@
 <?php
-$host = 'localhost';
-$db = 'Factory';
-$user = 'root';
-$pass = '';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Factory";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+// Create connection
+$conn = new mysqli($servername, $username, $password,  $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
